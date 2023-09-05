f2c3e432b8cfe5cfedca654fc2f1a3b288ac9741
Ilya Bagrov <ilya.b.only@gmail.com>
2023-09-05 23:38:25 +0300

    Update view functions to class based views

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
-def index(request):
-    return render(request, 'article-index.html', context={'name': 'article'})
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'article-index.html', context={'name': 'article'})
+# def index(request):
+#     return render(request, 'article-index.html', context={'name': 'article'})
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World'
-    })
+class MainPageView(TemplateView):
+
+    template_name = 'index.html'
+
+    def index(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
+    # def index(request):
+    #     return render(request, 'index.html', context={
+    #         'who': 'World'
+    #     })
 
 
 def about(request):
