71067795d18433f38d9f7ea1b1535c3888338f8e
Ilya Bagrov <ilya.b.only@gmail.com>
2023-09-06 23:19:56 +0300

    Update routing in article app

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from django.http.response import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
 
 # Create your views here.
 class ArticleView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'article-index.html', context={'name': 'article'})
+        return redirect(reverse('index', kwargs={'tag': 'python', 'article_id': 42}))
+        # return render(request, 'article-index.html', context={'name': 'article'})
 # def index(request):
 #     return render(request, 'article-index.html', context={'name': 'article'})
+
+
+def index(request, tag, article_id):
+    return HttpResponse(f'Статья номер {article_id}. Тег {tag}')
