43d585dace84df4bf5e13fcf6e02aea2a8404091
Ilya Bagrov <ilya.b.only@gmail.com>
2023-09-11 00:46:01 +0300

    Add admin panel

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
+
 
 # Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp')
+    search_fields = ['name', 'body']
+    list_filter = (('timestamp', admin.DateFieldListFilter),)
