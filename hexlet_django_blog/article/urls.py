a7098eb47d9e5d9f170ec4406b29a3b1b2b7850d
Ilya Bagrov <ilya.b.only@gmail.com>
2023-09-11 21:27:08 +0300

    Add list of articles at main article page

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('', views.ArticleView.as_view()),
-    path('<str:tag>/<int:article_id>', views.index, name='index'),
+    path('', views.IndexView.as_view()),
+    path('<str:tag>/<int:article_id>', views.article_view, name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.views import View
 from django.http.response import HttpResponse
-from django.shortcuts import redirect
-from django.urls import reverse
+# from django.shortcuts import redirect
+# from django.urls import reverse
+from hexlet_django_blog.article.models import Article
 
 # Create your views here.
-class ArticleView(View):
-
-    def get(self, request, *args, **kwargs):
-        return redirect(reverse('index', kwargs={'tag': 'python', 'article_id': 42}))
-        # return render(request, 'article-index.html', context={'name': 'article'})
 # def index(request):
 #     return render(request, 'article-index.html', context={'name': 'article'})
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        # return render(request, 'article-index.html', context={
+        #     'name': 'article'
+        # })
+        # return redirect(reverse('article', kwargs={
+            # 'tag': 'python', 'article_id': 42
+        # }))
+        articles = Article.objects.all()
+        return render(request, 'article-index.html', context={
+            'articles': articles
+        })
 
 
-def index(request, tag, article_id):
+def article_view(request, tag, article_id):
     return HttpResponse(f'Статья номер {article_id}. Тег {tag}')
