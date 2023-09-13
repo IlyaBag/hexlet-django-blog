c02cc16b774664140e434daaf4ec3bddc09b73c4
Ilya Bagrov <ilya.b.only@gmail.com>
2023-09-13 21:18:16 +0300

    Update article main page with reverse routing

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,5 +5,5 @@ from hexlet_django_blog.article import views
 urlpatterns = [
     path('', views.IndexView.as_view()),
     # path('<str:tag>/<int:article_id>', views.article_view, name='article'),
-    path('<int:article_id>', views.ArticleView.as_view()),
+    path('<int:article_id>', views.ArticleView.as_view(), name='article_view'),
 ]
